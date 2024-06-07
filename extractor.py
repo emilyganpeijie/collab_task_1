f9e179a8d08474d03022ba0bf112d473e5cd4ddc
emilyganpeijie <162313248+emilyganpeijie@users.noreply.github.com>
2024-06-08 00:59:14 +0800

    Update extractor.py
    Mandarin "、" problem fixed

--- extractor.py
+++ extractor.py
@@ -1,11 +1,11 @@
 import re
 
-with open("ke_yi_raw.txt", "r", encoding="utf-8") as f:     #read in file to raw_data
+with open("raw.txt", "r", encoding="utf-8") as f:     #read in file to raw_data
     raw_data = f.read()
     
 input_str = re.sub("\n|more|\t", '', raw_data)       #remove \n, \t, and "more"
 
-pattern = re.compile(r'\w*可以\w*')        #compile search pattern
+pattern = re.compile(r'\w*可以[、\w]*')        #compile search pattern
 matches = pattern.findall(input_str)        #output list: matches
 
 with open("ke_yi_processed.txt", "w") as f:     #write matches into txt file named "ke_yi_processed.txt"
